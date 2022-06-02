c8f2ed3e913065ca8823fdae1cac11ab87315f4f
Aris <ariscjj@outlook.com>
2022-06-02 15:53:03 +0100

    finished webpage, may add addd-ons later

---
+++ script.js
@@ -0,0 +1,51 @@
+const movies = [
+ {title: 'Harry Potter', explanation: 'This movie is about a dude with a stick...', hint: 'It\'s Magic'},
+ {title: 'Just Go With It', explanation: 'This movie is about people who go on holiday...', hint: 'Adam, Drew and Jennifer'},
+ {title: 'Never Back Down', explanation: 'This movie is about two guys with daddy issues who beat each other up...', hint: 'Kanye West - Stronger'},
+ {title: 'Spongebob Squarepants', explanation: 'This movie is about a rectangle...', hint: 'It\'s a cartoon'},
+ {title: '50 First Dates', explanation: 'This movie is about a chick, she has the worst memory...', hint: '50 times...'},
+ {title: 'Cars', explanation: 'In this movie, car go fast...', hint: 'Kachow'},
+ {title: 'Spiderman', explanation: 'In this movie this guy just does not pay his rent, no matter how many times the landlord asks...', hint: 'Peta-Paka'},
+ {title: 'The Wolf Of Wall Street', explanation: 'In this movie there\'s like illegal stuff, lots of money, and a blonde chick...', hint: 'HAWOOooooooooooo'},
+ {title: 'Inception', explanation: 'In this movie everyone is like sleeping all the time...', hint: 'Dreams...'},
+ {title: 'Peter Pan', explanation: 'In this movie some kids die and an angel escorts them to heaven...', hint: 'Always flying, cuz he neverlands'},
+ {title: 'The Lord Of The Rings', explanation: 'In this movie some small guys go for a walk...', hint: 'You will not vacate past this exact position'}
+];
+
+var explanation = document.getElementById("explanation"); 
+var hint = document.getElementById("hint"); 
+var btn_hint = document.getElementById("btn-hint"); 
+var form = document.getElementById("form"); 
+var guess = document.getElementById("guess");
+
+var mov = movies[Math.floor(Math.random()* movies.length)];
+
+
+
+document.addEventListener('DOMContentLoaded',(e) => {
+  explanation.innerHTML = mov.explanation; 
+});
+
+btn_hint.addEventListener("click",(e) => {
+  e.preventDefault();
+  hint.innerHTML = mov.hint; 
+  hint.classList.remove('hide'); 
+});
+
+form.addEventListener("submit", (e)=> {
+  e.preventDefault();
+  var elem = document.createElement('div'); 
+    elem.classList.add('alert'); 
+
+  if (guess.value.toLowerCase() == mov.title.toLowerCase()){
+    elem.classList.add('alert-success'); 
+    elem.innerHTML = "Correct!"; 
+  } 
+  else{ 
+    elem.classList.add('alert-danger'); 
+    elem.innerHTML = "Sorry, try again"; 
+  }
+
+  output.appendChild(elem)
+
+}); 
